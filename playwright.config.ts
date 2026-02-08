6d6114fb5d0c6fde21db22edf36f1735fc7bf10d
shidurbaba <30811546+shidurbaba@users.noreply.github.com>
2026-02-08 12:48:58 -0500

    feat: update timeout settings and add input fields assertions test

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 10000,
+ // timeout: 10000,
   globalTimeout: 60000,
-  expect:{ timeout: 2000},
+  expect:{ timeout: 5000},
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -32,9 +32,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: false,
     trace: 'on-first-retry',
     //actionTimeout: 5000,
-    navigationTimeout: 5000
+    //navigationTimeout: 5000
   },
 
   /* Configure projects for major browsers */
